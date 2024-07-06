1d34acf308e4df5ce73f3d152efe4a4d942e3884
amrdiab95 <50382915+amrdiab95@users.noreply.github.com>
2024-07-06 18:06:05 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function convertdata(){
+
+    var myValue = document.getElementById("input").value ;
+    var result = document.getElementById("result");
+    result.innerHTML= myValue * 50 +"Egyptian Pound"; 
+    return false ; 
+}
